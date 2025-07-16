f65eb67336a31103e0bac632f166ee1b54095ffe
victor <martinelli.ads@gmail.com>
2025-07-15 23:41:41 -0300

    corrigido o problema de duplicação do header

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import { Header } from '../../components/Layout/Header';
-import { Footer } from '../../components/Layout/Footer';
 import { SearchSection } from '../../components/Home/SearchSection';
 import { SolutionsSection } from '../../components/Home/SolutionsSection';
 import { OfficialStores } from '../../components/Home/OfficialStores';
@@ -11,15 +9,11 @@ import { ServicesSection } from '../../components/Home/ServicesSection';
 export const Home: React.FC = () => {
   return (
     <Box>
-      <Header />
-      <Box sx={{ mt: 7 }}> {/* Espaço para o header fixo */}
-        <SearchSection />
-        <SolutionsSection />
-        <OfficialStores />
-        <FeaturedSection />
-        <ServicesSection />
-      </Box>
-      <Footer />
+      <SearchSection />
+      <SolutionsSection />
+      <OfficialStores />
+      <FeaturedSection />
+      <ServicesSection />
     </Box>
   );
 };
